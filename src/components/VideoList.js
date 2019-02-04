f72697739a92917fff3a192ae0f9bcb26cb73dfb
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-04 12:37:54 -0200

    Fix list key

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 export default ({ videos, onVideoSelect }) => {
-  const videoList = videos.map((video) => {
-    return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
+  const videoList = videos.map((video, index) => {
+    return <VideoItem key={index} video={video} onVideoSelect={onVideoSelect} />
   });
   return <div className="ui relaxed divided list">{videoList}</div>;
 }
