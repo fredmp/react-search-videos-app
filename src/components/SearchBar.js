3e795bb6d6868e459d753edcbd8d52e70e51d659
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-04 12:08:57 -0200

    Add search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ export default class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state.term);
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
