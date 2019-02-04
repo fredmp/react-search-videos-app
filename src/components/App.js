627ceedc7a070df7155e72c40ce59097638dff22
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-04 12:09:39 -0200

    Add handler functions to App js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from "./VideoDetail";
+import YouTube from '../api/YouTube';
 
 export default class App extends React.Component {
+  state = { videos: [], selected: null };
+
+  componentDidMount() {
+    this.onTermSubmit('trending');
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await YouTube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: response.data.items,
+      selected: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selected: video });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selected} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
